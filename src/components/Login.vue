69e74891805679eb544b89074c2affb5a690b02f
LukeErdy <112845457+LukeErdy@users.noreply.github.com>
2025-04-04 21:46:30 +0000

    Add files via upload

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app theme="light">
+    <v-main>
+      <v-container class="ma-3 pa-3 d-flex justify-center">
+        <v-card class="pa-5" style="max-width: 500px; width: 100%;">
+          <h2 class="text-h5 mb-4">Log in or Create Account</h2>
+          <v-text-field label="Email" v-model="email" type="email" outlined dense />
+          <v-text-field label="Password" v-model="password" type="password" outlined dense />
+          <v-btn color="primary" class="mt-4" @click="submit">Submit</v-btn>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { ref, onMounted, nextTick } from "vue";
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const routeTo = (where) => {
+  router.push(where);
+};
+
+const email = ref('')
+const password = ref('')
+
+const submit = () => {
+  console.log('Login with:', email.value, password.value)
+}
+</script>
+  

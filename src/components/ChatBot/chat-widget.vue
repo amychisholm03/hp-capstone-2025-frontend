ded28759fb46e6829e1c9330274fbbbf8374566f
Amy Chisholm <amy.chisholm@wsu.edu>
2025-04-10 13:14:29 -0700

    chatbot now auto populates name and email

--- src/components/ChatBot/chat-widget.vue
+++ src/components/ChatBot/chat-widget.vue
@@ -1,18 +1,22 @@
 <template>
-    <LiveChatWidget license="19113362" group="0" visibility="minimized" customer-name=""
-    :customer-email="email" />
+    <LiveChatWidget license="19113362" group="0" visibility="minimized" :customer-name="name"
+        :customer-email="email" />
 </template>
 
 <script setup>
-    import {ref, watch} from 'vue';
-    import {
-        LiveChatWidget
-    } from '@livechat/widget-vue'
-    import { useUserStore } from '@/stores/user';
-    const userStore = useUserStore();
-    const email = ref('');
+import { ref, watch } from 'vue';
+import {
+    LiveChatWidget
+} from '@livechat/widget-vue'
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore();
+const email = ref('help@hp.com');
+const name = ref('help')
 
-    watch(userStore.email, (newEmail) => {
-       email.value = newEmail;
-    }, { immediate: true });
+watch(userStore, (newUserStore) => {
+    if (newUserStore.email) {
+        email.value = newUserStore.email;
+        name.value = newUserStore.email.split('@')[0];
+    }
+}, { immediate: true });
 </script>

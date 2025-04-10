9695c384648a403287124ec046ec0fed6165ab31
Amy Chisholm <amy.chisholm@wsu.edu>
2025-04-10 13:03:26 -0700

    name change

--- src/components/ChatBot/chat-widget.vue
+++ src/components/ChatBot/chat-widget.vue
@@ -1,70 +1,18 @@
 <template>
-  <LiveChatWidget 
-    license="19113362" 
-    group="0" 
-    visibility="minimized" 
-    customer-name="John Coke"
-    :customer-email="userStore.name"
-    @change-group="handleChangeGroup"
-    @new-event="handleNewEvent" 
-    @form-submitted="handleFormSubmitted"
-    @rating-submitted="handleRatingSubmitted" 
-  />
+    <LiveChatWidget license="19113362" group="0" visibility="minimized" customer-name=""
+    :customer-email="name" />
 </template>
 
-<script>
-import { ref } from 'vue'
-import {
-    LiveChatWidget,
-    useWidgetState,
-    useWidgetIsReady,
-    useWidgetChatData,
-    useWidgetGreeting,
-    useWidgetCustomerData,
-} from '@livechat/widget-vue'
-import { useUserStore } from '@/stores/user';
-const userStore = useUserStore();
-
-export default {
-    name: 'chat-widget', 
-    components: {
-        // eslint-disable-next-line vue/component-options-name-casing
+<script setup>
+    import {ref, watch} from 'vue';
+    import {
         LiveChatWidget
-    },
-    setup() {
-        const group = ref('0')
-        // const chatData = useWidgetChatData()
-        // const greeting = useWidgetGreeting()
-        // const widgetState = useWidgetState()
-        // const isWidgetReady = useWidgetIsReady()
-        // const customerData = useWidgetCustomerData()
+    } from '@livechat/widget-vue'
+    import { useUserStore } from '@/stores/user';
+    const userStore = useUserStore();
+    const name = ref('');
 
-        return {
-            group,
-            // chatData,
-            // greeting,
-            // widgetState,
-            // isWidgetReady,
-            // customerData
-        }
-    },
-    methods: {
-        stringify(value) {
-            return JSON.stringify(value, null, 2)
-        },
-        handleChangeGroup() {
-            this.group = this.group === '0' ? '1' : '0'
-        },
-        async handleNewEvent(event) {
-            console.log('LiveChatWidget -> onNewEvent: ', this.stringify(event))
-            //console.log('chatdata: ', this.stringify(this.chatData))
-        },
-        handleFormSubmitted(form) {
-            console.log('LiveChatWidget -> onFormSubmitted', this.stringify(form))
-        },
-        handleRatingSubmitted(rating) {
-            console.log('LiveChatWidget -> onRatingSubmitted', this.stringify(rating))
-        }
-    }
-}
+    watch(userStore.name, (newName) => {
+       name.value = newName;
+    }, { immediate: true });
 </script>
